0610b51eacd12295368c5564e280b591d9d0e6d1
Hanii <you@example.com>
2017-12-27 15:55:36 +0200

    modify login2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::API
   require 'json_web_token'
+  def authenticate_request!
+    #unless user_present?
+    if !current_user.present?
+      render json: { message: 'You must be authenticated first'}, status: :unauthorized
+    end
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { message: 'You must relog'}, status: :unauthorized
+  end
+
+  private
+  def jwt_token
+    #@payload_token || = if request.headers['Authentication-Token'].present?
+                         #request.headers['Authentication-Token'].split('.').last
+                       #end
+    @jwt_token ||= request.headers['Authentication-Token']
+  end
+
+  def session_info
+    @session_info ||= JsonWebToken.decode(jwt_token)
+  end
+
+  def current_user
+    @current_user ||= Consumer.find session_info[:consumer_id]
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
+
 end
