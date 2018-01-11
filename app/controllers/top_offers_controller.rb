d601a1008021abbba48576ad59541ac836f602a1
Hanii <you@example.com>
2018-01-11 20:12:22 +0200

    merge conflicts

--- app/controllers/top_offers_controller.rb
+++ app/controllers/top_offers_controller.rb
@@ -1,7 +1,7 @@
 class TopOffersController < ApplicationController
   def fetchTopOffers
-  	@offers = Offer.limit(3).joins(:buying_consumers).group("offers.id").order("count(offers.id) DESC")
-  
+  	@offers = Offer.where(status: 'progress').limit(3).joins(:buying_consumers).group("offers.id").order("count(offers.id) DESC")
+
   	# @offers= Offer.limit(3).joins(:buying_consumers).group('offers.id').order('COUNT(buying_consumers.id) DESC')
 
    end
--- app/views/offers/_offer.json.jbuilder
+++ app/views/offers/_offer.json.jbuilder
@@ -3,4 +3,5 @@ json.product_name offer.vendor_product.product.name
 json.thumbnail "http://localhost:3000#{offer.thumbnail}"
 json.seller offer.vendor.name
 json.progress offer.progress
+json.buying_consumers offer.buying_consumers.count
  # json.product_id offer.vendor_product.product.id
